96fe8ed9ed2217d23108f7f4b1699160a727285f
belladev250 <lissadev17@gmail.com>
2025-05-14 11:18:20 +0200

    Fix: clean database config for PostgreSQL with SSL

--- config/database.ts
+++ config/database.ts
@@ -27,15 +27,9 @@ export default ({ env }) => {
         client: 'postgres',
         connection: {
           connectionString: env('DATABASE_URL'),
+          ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
 
-          ssl: { rejectUnauthorized: false },
-          schema: env('DATABASE_SCHEMA', 'public'),
         },
-        pool: {
-          min: env.int('DATABASE_POOL_MIN', 2),
-          max: env.int('DATABASE_POOL_MAX', 10),
-        },
-        acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
       },
     },
     sqlite: {
