08008040ff00436c377a438f297f6485080d0d31
belladev250 <lissadev17@gmail.com>
2025-05-14 11:45:42 +0200

    added pg

--- config/database.ts
+++ config/database.ts
@@ -1,10 +1,18 @@
-import path from 'path';
+import path from "path";
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT') || 'postgres';
+  const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
+    postgres: {
+      client: 'postgres',
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
+      },
+    },
     mysql: {
+      client: 'mysql',
       connection: {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
@@ -12,26 +20,12 @@ export default ({ env }) => {
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    postgres: {
-      connection: {
-        client: 'postgres',
-        connection: {
-          connectionString: env('DATABASE_URL'),
-          ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
         },
       },
     },
     sqlite: {
+      client: 'sqlite',
       connection: {
         filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       },
@@ -41,7 +35,6 @@ export default ({ env }) => {
 
   return {
     connection: {
-      client,
       ...connections[client],
       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
